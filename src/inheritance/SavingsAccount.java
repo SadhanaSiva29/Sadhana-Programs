c9db9db23f4caacc5a2fe7edf407687e25432fc5
SadhanaSiva29 <sadhanasivakumar328@gmail.com>
2024-08-06 15:20:22 +0530

    Inheritance Task

--- src/inheritance/SavingsAccount.java
+++ src/inheritance/SavingsAccount.java
@@ -4,7 +4,7 @@ public class SavingsAccount extends BankAccount{
     private double interestRate;
     public SavingsAccount(String accountNumber,double balance, int year,double amount, double interestRate){
         super( accountNumber,balance);
-        double interest= amount*year*interestRate;
+        double interest= amount*year*interestRate/100;
         System.out.println("Interest: "+interest);
     }
     public static void main(String[] args) {
