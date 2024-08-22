fd383ca1779c516b6a1672533f0e39975f6aea67
SadhanaSiva29 <sadhanasivakumar328@gmail.com>
2024-08-22 14:59:37 +0530

    delete

--- src/ControlFlow/PalindromeCheck.java
+++ src/ControlFlow/PalindromeCheck.java
@@ -2,19 +2,20 @@ package ControlFlow;
 
 public class PalindromeCheck {
     public static void main(String[] args) {
-        String[] str=new String[]{"racecar","man","madam","question"};
+        String[] str = new String[]{"racecar", "man", "madam", "question"};
         String strrev;
         char ch;
         int i;
         int j;
-        for( i = 0;i<4;i++) {
+        for (i = 0; i < 4; i++) {
             strrev = "";
             for (j = 0; j < str[i].length(); j++) {
                 ch = str[i].charAt(j);
                 strrev = ch + strrev;
             }
-            if(str[i].equals(strrev)){
-                System.out.println(str[i]+" is a palindrome");
+            if (str[i].equals(strrev)) {
+                System.out.println(str[i] + " is a palindrome");
             }
         }
     }
+}
