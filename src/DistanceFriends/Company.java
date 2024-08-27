d48b9ee5b7453529d72af9433d13c751cd84693a
SadhanaSiva29 <150834079+SadhanaSiva29@users.noreply.github.com>
2024-08-27 16:57:35 +0530

    Update Company.java

--- src/DistanceFriends/Company.java
+++ src/DistanceFriends/Company.java
@@ -14,28 +14,3 @@ public class Company extends Contact {
                 "\nEstablished Year: " + establishedYear);
     }
 }
-//Company(){
-//
-//
-//}
-//
-//Company(int gst, int establishedYear){
-//
-//    this.gst = gst;
-//    this.establishedYear = establishedYear;
-//}
-//
-//public Company getObjet(int gst, int establishedYear){
-//
-//    return new Company(gst,establishedYear);
-//}
-//
-//
-//
-//public static void main(String[] args) {
-//
-//    Company company = new Company();
-//
-//    Company company1 = company.getObjet(445,2024);
-//
-//}
