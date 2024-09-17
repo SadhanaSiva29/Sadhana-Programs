474f1fb738687ddbf7ad69e7520d46e327002d49
SadhanaSiva29 <sadhanasivakumar328@gmail.com>
2024-09-17 16:44:52 +0530

    comments verification1

--- src/regextask/PhoneNumber.java
+++ src/regextask/PhoneNumber.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumber {
-    public static String extractPhoneNumbers(String text) {
+    public  String extractPhoneNumbers(String text) {
         String phoneRegex = "\\(\\+\\d{2}\\)\\s\\d{5}-\\d{5} | \\d{3}-\\d{3}-\\d{4}";
         Pattern pattern = Pattern.compile(phoneRegex);
         Matcher matcher = pattern.matcher(text);
@@ -19,7 +19,8 @@ public class PhoneNumber {
     }
 
     public static void main(String[] args) {
-        System.out.println(extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
-        System.out.println(extractPhoneNumbers("No phone number here!"));
+        PhoneNumber phoneNumber = new PhoneNumber();
+        System.out.println(phoneNumber.extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
+        System.out.println(phoneNumber.extractPhoneNumbers("No phone number here!"));
     }
 }
