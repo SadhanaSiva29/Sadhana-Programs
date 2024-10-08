24fee683f4d6cf9ea8d7ad786c72cfe4ae0c8b06
SadhanaSiva29 <sadhanasivakumar328@gmail.com>
2024-10-08 11:13:32 +0530

    CreateEmployee1

--- src/employeejdbc/CreateEmployee.java
+++ src/employeejdbc/CreateEmployee.java
@@ -6,13 +6,12 @@ public class CreateEmployee {
     public String employeeID;
     public String firstName;
     public String lastName;
-    public Date dob;
+    public String dob;
     public String gender;
     public String emailAdd;
     public String phoneNo;
     public String address;
-    public
-    CreateEmployee(String employeeID, String firstName, String lastName, Date dob, String gender, String emailAdd,
+    public CreateEmployee(String employeeID, String firstName, String lastName, String dob, String gender, String emailAdd,
                                  String phoneNo, String address){
         this.employeeID = employeeID;
         this.firstName = firstName;
@@ -22,5 +21,19 @@ public class CreateEmployee {
         this.emailAdd = emailAdd;
         this.phoneNo = phoneNo;
         this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "\nEmployee ID: "+ employeeID + "\nFirst name: "+firstName+"\nLast name: "+lastName+"\nDate of birth: "+dob+"\nEmail ID: "
+                +employeeID +"\nPhone No.: "+phoneNo + "\nAddress: "+ address;
+    }
+
+    public static void main(String[] args) {
+        CreateEmployee createEmployee = new CreateEmployee("CS01","S","Sadhana",
+                "29/9/2004","female","sdfga@gmail.com",
+                "9023457384","Kanchipuram");
+        System.out.println(createEmployee);
+
     }
 }
